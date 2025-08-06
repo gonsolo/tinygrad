833f4a17bd27253ec9072336c555197129b0253c
Gon Solo <gonsolo@gmail.com>
2025-08-06 14:39:14 +0200

    Adapt for new mesa3d.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all:
+	LD_PRELOAD=/usr/lib/libasan.so.8 ASAN_OPTIONS=detect_leaks=0 PYTHONPATH=/home/gonsolo/work/mesa/build/src/compiler \
 	DEBUG=2 NAK=1 pytest -s test/test_setitem.py::TestSetitemLoop::test_arange
 edit:
 	vi tinygrad/runtime/ops_nak.py
+gdb:
+	DEBUG=2 NAK=1 gdb -ex=r --directory=../python-mesa3d/subprojects --args python -m pytest -s test/test_setitem.py::TestSetitemLoop::test_arange
 .PHONY: all edit
--- tinygrad/runtime/ops_nak.py
+++ tinygrad/runtime/ops_nak.py
@@ -5,17 +5,34 @@ from tinygrad.dtype import dtypes
 from tinygrad.engine.jit import MultiGraphRunner
 from tinygrad.uop.ops import Ops, UOp
 
-import mesa3d
-
-
 class NakRenderer(Renderer):
   device = "NAK"
+
+  def get_or_create_ssa_def(self, uop, builder, ssa_defs):
+    ssa_def = ssa_defs.get(uop)
+    if ssa_def is not None:
+      return ssa_def
+
+    if uop.op == Ops.CONST:
+      const_val = uop.arg
+      const_dtype = uop.dtype
+
+      if const_dtype == dtypes.int:
+        const_def = mesa3d.nir_imm_int(builder, int(const_val))
+      elif const_dtype == dtypes.float:
+        const_def = mesa3d.nir_imm_float(builder, float(const_val))
+      else:
+        raise NotImplementedError(f"Unsupported constant type: {const_dtype}")
+
+      ssa_defs[uop] = const_def
+      return const_def
+
+    raise ValueError(f"SSA definition for UOp {uop} not found and cannot be created.")
 
   def render(self, uops: list) -> str:
     stage = mesa3d.gl_shader_stage.COMPUTE
     options = mesa3d.nir_shader_compiler_options()
     builder = mesa3d.nir_builder_init_simple_shader(stage, options, "simple")
-    main = mesa3d.nir_shader_get_function_for_name(builder.shader, "main");
     mesa3d.glsl_type_singleton_init_or_ref()
 
     ssa_defs = {}
@@ -23,90 +40,90 @@ class NakRenderer(Renderer):
     unhandled_uops = set()
     printed = False
 
+    #for idx, uop in enumerate(uops):
+    #  print(f"UOp at index {idx}: {uop.op}, dtype: {uop.dtype}, arg: {uop.arg}, src: {uop.src}")
+
     for uop in uops:
-        if uop.op == Ops.DEFINE_GLOBAL:
-            var_type = uop.dtype
-            var_binding = uop.arg
-            var_size = var_type.count
+      if uop.op == Ops.DEFINE_GLOBAL:
+        var_type = uop.dtype
+        var_binding = uop.arg
+        var_size = var_type.count
 
-            if var_type.base == dtypes.int:
-                glsl_base_type = mesa3d.glsl_int_type()
-            elif var_type.base == dtypes.float:
-                glsl_base_type = mesa3d.glsl_float_type()
-            else:
-                raise NotImplementedError(f"Unsupported dtype: {var_type.base}")
+    #    if var_type.base == dtypes.int:
+    #      glsl_base_type = mesa3d.glsl_int_type()
+    #    elif var_type.base == dtypes.float:
+    #      glsl_base_type = mesa3d.glsl_float_type()
+    #    else:
+    #      raise NotImplementedError(f"Unsupported dtype: {var_type.base}")
 
-            nir_type = mesa3d.glsl_array_type(glsl_base_type, var_size, 0)
-            nir_var = mesa3d.nir_variable_create(builder.shader,
-                                                 mesa3d.nir_var_mem_ssbo,
-                                                 nir_type,
-                                                 f"ssbo_var_{var_binding}")
-            nir_var.data.binding = var_binding
-            nir_var.data.explicit_binding = True
+    #    nir_type = mesa3d.glsl_array_type(glsl_base_type, var_size, 0)
+    #    nir_var = mesa3d.nir_variable_create(builder.shader,
+    #                                         mesa3d.nir_var_mem_ssbo,
+    #                                         nir_type,
+    #                                         f"ssbo_var_{var_binding}")
+    #    nir_var.data.binding = var_binding
+    #    nir_var.data.explicit_binding = True
 
-            nir_vars[var_binding] = nir_var
+    #    nir_vars[var_binding] = nir_var
 
-        elif uop.op == Ops.CONST:
-            const_val = uop.arg
-            const_dtype = uop.dtype
+    #  elif uop.op == Ops.INDEX:
+    #    ssbo_uop = uop.src[0]
+    #    index_uop = uop.src[1]
 
-            if const_dtype == dtypes.int:
-                const_def = mesa3d.nir_imm_int(builder, int(const_val))
-            elif const_dtype == dtypes.float:
-                const_def = mesa3d.nir_imm_float(builder, float(const_val))
-            else:
-                raise NotImplementedError(f"Unsupported constant type: {const_dtype}")
+    #    ssbo_var = nir_vars.get(ssbo_uop.arg)
+    #    if ssbo_var is None:
+    #      raise ValueError(f"SSBO variable for binding {ssbo_uop.arg} not found.")
 
-            ssa_defs[uop] = const_def
+    #    index_def = self.get_or_create_ssa_def(index_uop, builder, ssa_defs)
 
-        #elif uop.op == Ops.INDEX:
-        #    ssbo_uop = uop.src[0]
-        #    index_uop = uop.src[1]
+    #    ssbo_type = ssbo_var.type
+    #    if ssbo_type.is_array():
+    #      element_type = ssbo_type.array
+    #      while element_type and element_type.is_array():
+    #        element_type = element_type.array
+    #    else:
+    #      element_type = ssbo_type
 
-        #    # Look up the SSBO variable by its binding
-        #    ssbo_var = nir_vars.get(ssbo_uop.arg)
-        #    if ssbo_var is None:
-        #        raise ValueError(f"SSBO variable for binding {ssbo_uop.arg} not found.")
+    #    element_size = mesa3d.glsl_get_explicit_size(element_type, True)
+    #    offset_def = mesa3d.nir_imul_imm(builder, index_def, element_size)
 
-        #    # Get the SSA definition for the index
-        #    index_def = ssa_defs.get(index_uop)
-        #    if index_def is None:
-        #        raise ValueError(f"SSA definition for index UOp not found.")
+    #    num_components = mesa3d.glsl_get_vector_elements(element_type)
+    #    bit_size = mesa3d.glsl_get_bit_size(element_type)
 
-        #    ssbo_type = ssbo_var.type
-        #    if ssbo_type.is_array():
-        #        array_element_type = ssbo_type.array
-        #        while array_element_type and array_element_type.is_array():
-        #            array_element_type = array_element_type.array
-        #        if array_element_type:
-        #            element_type = array_element_type
-        #    else:
-        #        element_type = ssbo_type
-        #    # Get the size of a single element in bytes
-        #    element_size = mesa3d.glsl_get_explicit_size(element_type, True)
+    #    loaded_val_def = mesa3d.nir_load_ssbo(builder, num_components, bit_size,
+    #                                          mesa3d.nir_load_var(builder, ssbo_var),
+    #                                          offset_def)
 
-        #    # Calculate the byte offset: index * element_size
-        #    offset_def = mesa3d.nir_imul_imm(builder, index_def, element_size)
+    #    ssa_defs[uop] = loaded_val_def
 
-        #    # Load the value from the SSBO at the calculated offset
-        #    num_components = mesa3d.glsl_get_vector_elements(element_type)
-        #    bit_size = mesa3d.glsl_get_bit_size(element_type)
+    #  elif uop.op == Ops.STORE:
+    #    dest_uop = uop.src[0]
+    #    src_uop = uop.src[1]
 
-        #    loaded_val_def = mesa3d.nir_load_ssbo(builder, num_components, bit_size,
-        #                                          mesa3d.nir_load_var(builder, ssbo_var),
-        #                                          offset_def)
+    #    dest_def = self.get_or_create_ssa_def(dest_uop, builder, ssa_defs)
+    #    src_def = self.get_or_create_ssa_def(src_uop, builder, ssa_defs)
 
-        #    # Store the loaded value's SSA definition for later use
-        #    ssa_defs[uop] = loaded_val_def
+    #  elif uop.op == Ops.SINK:
+    #    for src_uop in uop.src:
+    #      if src_uop.op == Ops.STORE:
+    #        dest_uop = src_uop.src[0]
+    #        src_uop_val = src_uop.src[1]
 
-        else:
-            if not printed:
-                print(uop)
-                printed = True
-            unhandled_uops.add(uop.op)
+    #        dest_def = self.get_or_create_ssa_def(dest_uop, builder, ssa_defs)
+    #        src_def = self.get_or_create_ssa_def(src_uop_val, builder, ssa_defs)
 
-    #print(unhandled_uops)
-    mesa3d.nir_validate_shader(builder.shader, None)
+    #  else:
+    #    try:
+    #        ssa_def = self.get_or_create_ssa_def(uop, builder, ssa_defs)
+    #        if ssa_def:
+    #            ssa_defs[uop] = ssa_def
+    #    except ValueError:
+    #        if not printed:
+    #          print(uop)
+    #          printed = True
+    #        unhandled_uops.add(uop.op)
+
+    #mesa3d.nir_validate_shader(builder.shader, None)
     return "Ok"
 
 class NakProgram:
