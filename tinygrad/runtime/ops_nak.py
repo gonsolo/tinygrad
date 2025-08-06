d2efb9bc71797d1789eea2fc8022ae46ca9582df
Gon Solo <gonsolo@gmail.com>
2025-08-06 19:20:51 +0200

    Handle all ops.

--- tinygrad/runtime/ops_nak.py
+++ tinygrad/runtime/ops_nak.py
@@ -8,27 +8,6 @@ from tinygrad.uop.ops import Ops, UOp
 class NakRenderer(Renderer):
   device = "NAK"
 
-  def get_or_create_ssa_def(self, uop, builder, ssa_defs):
-    ssa_def = ssa_defs.get(uop)
-    if ssa_def is not None:
-      return ssa_def
-
-    if uop.op == Ops.CONST:
-      const_val = uop.arg
-      const_dtype = uop.dtype
-
-      if const_dtype == dtypes.int:
-        const_def = mesa3d.nir_imm_int(builder, int(const_val))
-      elif const_dtype == dtypes.float:
-        const_def = mesa3d.nir_imm_float(builder, float(const_val))
-      else:
-        raise NotImplementedError(f"Unsupported constant type: {const_dtype}")
-
-      ssa_defs[uop] = const_def
-      return const_def
-
-    raise ValueError(f"SSA definition for UOp {uop} not found and cannot be created.")
-
   def render(self, uops: list) -> str:
     stage = mesa3d.gl_shader_stage.COMPUTE
     options = mesa3d.nir_shader_compiler_options()
@@ -37,17 +16,10 @@ class NakRenderer(Renderer):
 
     ssa_defs = {}
     nir_vars = {}
-    unhandled_uops = set()
-    handled_uops = set()
-
-    print(f"Number of uops: {len(uops)}")
-    #for idx, uop in enumerate(uops):
-    #  print(f"UOp at index {idx}: {uop.op}, dtype: {uop.dtype}, arg: {uop.arg}, src: {uop.src}")
+    deref_instrs = {}
 
     for uop in uops:
       if uop.op == Ops.DEFINE_GLOBAL:
-        handled_uops.add(uop.op)
-
         var_type = uop.dtype
         var_binding = uop.arg
         var_size = var_type.count
@@ -66,88 +38,74 @@ class NakRenderer(Renderer):
                                              f"ssbo_var_{var_binding}")
         nir_var.data.binding = var_binding
         nir_var.data.explicit_binding = True
-
         nir_vars[var_binding] = nir_var
 
-      elif uop.op == Ops.CONST:
-        handled_uops.add(uop.op)
+      elif uop.op == Ops.SINK:
+        store_uop = uop.src[0]
+        if store_uop.op != Ops.STORE:
+            raise ValueError("SINK UOp's source is not a STORE operation.")
 
-        const_val = uop.arg
-        const_dtype = uop.dtype
+        dst_uop = store_uop.src[0]
+        src_uop = store_uop.src[1]
 
-        if const_dtype == dtypes.int:
-          const_def = mesa3d.nir_imm_int(builder, int(const_val))
-        elif const_dtype == dtypes.float:
-          const_def = mesa3d.nir_imm_float(builder, float(const_val))
-        else:
-          raise NotImplementedError(f"Unsupported constant type: {const_dtype}")
+        dst_deref = self._get_or_create_deref_instr(dst_uop, builder, deref_instrs, nir_vars, ssa_defs)
+        src_def = self._get_or_create_ssa_def(src_uop, builder, ssa_defs, deref_instrs, nir_vars)
 
-        ssa_defs[uop] = const_def
+        mesa3d.nir_store_deref(builder, dst_deref, src_def, 0xff)
 
-      elif uop.op == Ops.INDEX:
-        handled_uops.add(uop.op)
+    return "Ok"
+
+  def _get_or_create_deref_instr(self, uop, builder, deref_instrs, nir_vars, ssa_defs):
+    deref_instr = deref_instrs.get(uop)
+    if deref_instr is not None:
+        return deref_instr
+
+    if uop.op == Ops.INDEX:
         ssbo_uop = uop.src[0]
         index_uop = uop.src[1]
 
         ssbo_var = nir_vars.get(ssbo_uop.arg)
         if ssbo_var is None:
-          raise ValueError(f"SSBO variable for binding {ssbo_uop.arg} not found.")
+            raise ValueError(f"SSBO variable for binding {ssbo_uop.arg} not found.")
 
-        index_def = self.get_or_create_ssa_def(index_uop, builder, ssa_defs)
-
-        element_type = ssbo_var.type
-        while element_type.is_array():
-            element_type = element_type.array
-
-        element_size = mesa3d.glsl_get_explicit_size(element_type, True)
-        offset_def = mesa3d.nir_imul_imm(builder, index_def, element_size)
-
-        num_components = mesa3d.glsl_get_vector_elements(element_type)
-        bit_size = mesa3d.glsl_get_bit_size(element_type)
+        index_def = self._get_or_create_ssa_def(index_uop, builder, ssa_defs, deref_instrs, nir_vars)
 
         ssbo_deref = mesa3d.nir_build_deref_var(builder, ssbo_var)
-        array_deref = mesa3d.nir_build_deref_array(builder, ssbo_deref, index_def)
-        loaded_val_def = mesa3d.nir_load_deref(builder, array_deref)
+        deref_instr = mesa3d.nir_build_deref_array(builder, ssbo_deref, index_def)
 
-        ssa_defs[uop] = loaded_val_def
+        deref_instrs[uop] = deref_instr
+        return deref_instr
+    else:
+        raise NotImplementedError(f"Unsupported UOp type for dereference: {uop.op}")
 
-      elif uop.op == Ops.SINK:
-        unhandled_uops.add(uop.op)
-        pass
-      elif uop.op == Ops.LOAD:
-        unhandled_uops.add(uop.op)
-        pass
-      elif uop.op == Ops.STORE:
-        unhandled_uops.add(uop.op)
-        pass
-      elif uop.op == Ops.INDEX:
-        unhandled_uops.add(uop.op)
-        pass
-      elif uop.op == Ops.ADD:
-        unhandled_uops.add(uop.op)
-        pass
+  def _get_or_create_ssa_def(self, uop, builder, ssa_defs, deref_instrs, nir_vars):
+    ssa_def = ssa_defs.get(uop)
+    if ssa_def is not None:
+        return ssa_def
 
-    #  elif uop.op == Ops.STORE:
-    #    dest_uop = uop.src[0]
-    #    src_uop = uop.src[1]
+    if uop.op == Ops.CONST:
+        const_val = uop.arg
+        const_dtype = uop.dtype
+        if const_dtype == dtypes.int:
+            ssa_def = mesa3d.nir_imm_int(builder, int(const_val))
+        elif const_dtype == dtypes.float:
+            ssa_def = mesa3d.nir_imm_float(builder, float(const_val))
+        else:
+            raise NotImplementedError(f"Unsupported constant type: {const_dtype}")
 
-    #    dest_def = self.get_or_create_ssa_def(dest_uop, builder, ssa_defs)
-    #    src_def = self.get_or_create_ssa_def(src_uop, builder, ssa_defs)
+    elif uop.op == Ops.ADD:
+        src_defs = [self._get_or_create_ssa_def(src, builder, ssa_defs, deref_instrs, nir_vars) for src in uop.src]
+        ssa_def = mesa3d.nir_iadd(builder, src_defs[0], src_defs[1])
 
-    #  elif uop.op == Ops.SINK:
-    #    for src_uop in uop.src:
-    #      if src_uop.op == Ops.STORE:
-    #        dest_uop = src_uop.src[0]
-    #        src_uop_val = src_uop.src[1]
+    elif uop.op == Ops.LOAD:
+        deref_chain = self._get_or_create_deref_instr(uop.src[0], builder, deref_instrs, nir_vars, ssa_defs)
+        ssa_def = mesa3d.nir_load_deref(builder, deref_chain)
 
-    #        dest_def = self.get_or_create_ssa_def(dest_uop, builder, ssa_defs)
-    #        src_def = self.get_or_create_ssa_def(src_uop_val, builder, ssa_defs)
+    else:
+        raise NotImplementedError(f"Unsupported UOp type: {uop.op}")
 
-    print(f"Handled uops: {handled_uops}")
-    print(f"Unhandled uops: {unhandled_uops}")
-
-    #mesa3d.nir_validate_shader(builder.shader, None)
-    return "Ok"
+    ssa_defs[uop] = ssa_def
+    return ssa_def
 
 class NakProgram:
   def __init__(self, name:str, lib:bytes): pass
@@ -165,3 +123,4 @@ class NakGraph(MultiGraphRunner):
 
 class NakDevice(Compiled):
   def __init__(self, device:str): super().__init__(device, NakAllocator(self), NakRenderer(), Compiler(), NakProgram, NakGraph)
+
